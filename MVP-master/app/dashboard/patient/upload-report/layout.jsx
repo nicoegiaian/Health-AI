859e1f83e4bcdfa4fb477c356e911c595025da09
nicoegiaian <nicolas.egiaian@gmail.com>
2025-08-19 14:44:10 -0300

    Delete MVP-master directory

--- MVP-master/app/dashboard/patient/upload-report/layout.jsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Upload Medical Report - Medisynix",
-  description: "Upload and analyze medical images or lab reports using AI",
-};
-
-export default function UploadReportLayout({ children }) {
-  return children;
-}
--- MVP-master/app/page.js
+++
@@ -1,9 +0,0 @@
-import MedicalAssistant from './components/MedicalAssistant';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-4">
-      <MedicalAssistant />
-    </main>
-  );
-} 
